165e1963638464474005a41301f3ea55db95fd9e
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-02-19 09:02:12 +0700

    membuat input

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
     echo "Selamat datang di basic list"."\n";
     $list = [];
-    $opsi = ["Tambah List","Hapus list"];
+    $opsi = ["Tambah","Hapus"];
+    echo"========================="."\n";
     foreach($opsi as $i){
         echo $i."\n";
     }
+    echo"========================="."\n";
+    echo "Masukkan perintah yang kamu mau : ";
+    $pilihan = trim(fgets(STDIN));
+    if(strtolower($pilihan)=="tambah"){
+        echo "Kamu memilih opsi tambah";
+    }
+
+   
 ?>
